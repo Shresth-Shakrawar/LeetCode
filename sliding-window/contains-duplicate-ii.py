a0a456245e673b949841ca0a43de5c536d3cab96
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-06-15 17:10:28 +0530

    LC-contains-duplicate-ii [Runtime: 36 ms; Memory: 30.8 MB]

--- sliding-window/contains-duplicate-ii.py
+++ sliding-window/contains-duplicate-ii.py
@@ -1,16 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        indexMap={}       # Stores Last Index Where Number was Encountered{1:0,2:1,3:2}[1 encountered at 0]
-        for i,num in enumerate(nums):
-            if num in indexMap and i-indexMap[num] <=k:
+        L=0
+        window=set()
+        for R in range(len(nums)):
+            if R-L > k:
+                window.remove(nums[L])
+                L+=1
+            if nums[R] in window:
                 return True
-            else:
-                indexMap[num]= i
-        return False
+            window.add(nums[R])
+
+        return False    
             
-
-
-        
-        
-        
-        
