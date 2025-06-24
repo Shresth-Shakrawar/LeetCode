7367c2d7e26ecb9e82268851abe913b958c502d3
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-06-24 22:22:06 +0530

    LC-minimum-size-subarray-sum [Runtime: 12 ms; Memory: 28.4 MB]

---
+++ sliding-window/minimum-size-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        suma=0
+        l=0
+        minL = 100001
+        for r in range(len(nums)):
+            suma = suma + nums[r]
+            #print(f"start {l=} {r=} {suma=}")
+            while suma >= target :
+                minL = min( minL , (r-l) + 1 )
+                suma = suma - nums[l]
+                l += 1
+            #print(f"after {l=} {r=} {suma=} {minL=}")
+        if minL != 100001:
+            return minL
+        else:
+            return 0
