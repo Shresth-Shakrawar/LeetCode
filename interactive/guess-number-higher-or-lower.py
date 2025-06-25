ae6bc68d12a4e6ca99712f50da57cf2f6f6d5a6b
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-06-25 21:12:36 +0530

    LC-guess-number-higher-or-lower [Runtime: 39 ms; Memory: 17.7 MB]

--- interactive/guess-number-higher-or-lower.py
+++ interactive/guess-number-higher-or-lower.py
@@ -11,9 +11,10 @@ class Solution:
         r = n
         while l<=r:
             mid = (l+r) // 2
-            if guess(mid) == -1:
+            res = guess(mid)
+            if res == -1:
                 r = mid - 1
-            elif guess(mid) == 1:
+            elif res == 1:
                 l = mid + 1
             else:
                 return mid
