9c522cc0fc11d3b6ea6cb4ab193e40da074e3454
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-06-29 15:02:31 +0530

    LC-find-k-closest-elements [Runtime: N/A; Memory: N/A]

---
+++ array/find-k-closest-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if k == len(arr):
+            return arr
+        l , r = 0 , k - 1
+        while r < len(arr) - 1:
+            if abs(arr[r+1] - x) < abs(arr[l] - x) :
+                l += 1
+                r += 1
+            else:
+                break
+        return arr[l:r+1]
