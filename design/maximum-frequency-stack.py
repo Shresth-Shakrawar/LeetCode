fbb60034cd04d79023aff092f24d0e7e3cb9424b
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-07-01 14:43:32 +0530

    LC-maximum-frequency-stack [Runtime: 85 ms; Memory: 26.3 MB]

--- design/maximum-frequency-stack.py
+++ design/maximum-frequency-stack.py
@@ -1,17 +1,25 @@
 class FreqStack:
 
     def __init__(self):
-        self.cnt = defaultdict(int)
-        self.stack = []
+        self.maxFreq = 0
+        self.count = {}
+        self.stack = {}
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        self.cnt[val] += 1
+        newCount = self.count.get(val, 0) + 1
+        self.count[val] = newCount
+
+        if newCount > self.maxFreq:
+            self.stack[newCount] = []
+            self.maxFreq = newCount
+        
+        self.stack[newCount].append(val)
 
     def pop(self) -> int:
-        maxCnt = max(self.cnt.values())
-        i = len(self.stack) - 1
-        while self.cnt[self.stack[i]] != maxCnt:
-            i -= 1
-        self.cnt[self.stack[i]] -= 1
-        return self.stack.pop(i)
+        val = self.stack[self.maxFreq].pop()
+        self.count[val] -= 1
+        if not self.stack[self.maxFreq]:
+            self.maxFreq -= 1
+
+        return val
+        
