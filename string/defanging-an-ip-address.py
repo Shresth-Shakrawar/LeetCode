8f8d5ac8f160a0cab46bf2ef1d2f0745b70c53e3
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-07-01 22:52:18 +0530

    LC-defanging-an-ip-address [Runtime: 42 ms; Memory: 17.7 MB]

--- string/defanging-an-ip-address.py
+++ string/defanging-an-ip-address.py
@@ -1,5 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        l1 = address.split(".")
-        new_address = "[.]".join(l1)
-        return new_address
+        return address.replace('.','[.]')
