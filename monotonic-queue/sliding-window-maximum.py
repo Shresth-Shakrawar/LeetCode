14ab4c7519c683ff5dcad1cfd8f5eeb88312627a
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-07-01 21:19:28 +0530

    LC-sliding-window-maximum [Runtime: 193 ms; Memory: 31 MB]

--- monotonic-queue/sliding-window-maximum.py
+++ monotonic-queue/sliding-window-maximum.py
@@ -1,8 +1,21 @@
+import collections
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        l = 0
-        res = []
-        for r in range(k-1,len(nums)):
-            res.append(max(nums[l:r+1]))
-            l+=1
-        return res
+        deque = collections.deque() # indexes
+        output = []
+        l,r = 0,0
+        while r < len(nums):
+            while deque and nums[deque[-1]] < nums[r]:
+                deque.pop()
+            deque.append(r)
+
+            if l > deque[0]:
+                deque.popleft()
+
+            if (r+1) >= k:
+                output.append(nums[deque[0]])
+                l+=1
+            r+=1
+        return output
+
+            
