8bdb55235cc0238386d42e7ba92b2a65e815d626
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-07-02 18:10:05 +0530

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 17.7 MB]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -1,22 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = node = ListNode()
-
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        cur = result
+        
         while list1 and list2:
-            if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
-            else:
-                node.next = list2
+            if list1.val > list2.val:
+                cur.next = list2
                 list2 = list2.next
-            node = node.next
-
-        node.next = list1 or list2
-
-        return dummy.next
+            else:
+                cur.next = list1
+                list1 = list1.next
+            cur = cur.next
+        
+        if list1:
+            cur.next = list1
+        if list2:
+            cur.next = list2
+                
+        return result.next
