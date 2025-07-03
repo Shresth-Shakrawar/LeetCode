da953a47cf8458913e04c1ed7bc64e93bd7990f9
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2025-07-03 23:38:22 +0530

    LC-running-sum-of-1d-array [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ prefix-sum/running-sum-of-1d-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        rSum = [nums[0]]
+        for i in range(1,len(nums)):
+            rSum.append(rSum[i-1] + nums[i])
+        return rSum
