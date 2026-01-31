2c80b33ada6d92eb1aa43a8be2ca4a15cd1f59a7
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2026-01-31 18:29:19 +0530

    Add solution for 94. Binary Tree Inorder Traversal

---
+++ easy/94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -0,0 +1,11 @@
+        ans = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            ans.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return ans
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
