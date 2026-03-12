f49eb062a559eadae7819508914f9e2954b51da5
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2026-03-12 22:12:21 +0530

    Add solution for 213. House Robber II

---
+++ medium/213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,9 @@
+        for i in range(2,n):
+        dp[1] = max(nums[0], nums[1])
+        dp[0] = nums[0]
+        dp = [0]*n
+            return nums[0]
+        if n == 1:
+        n = len(nums)
+class Solution:
+    def robHouse(self, nums):
