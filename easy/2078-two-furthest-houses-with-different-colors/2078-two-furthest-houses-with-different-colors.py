9675496cc176b542b24731dc472ec38f4636fbf8
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2026-04-20 22:24:16 +0530

    Add solution for 2078. Two Furthest Houses With Different Colors

---
+++ easy/2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -0,0 +1,9 @@
+                left = i
+                break
+        for i in range(n - 1, -1, -1):
+        for i in range(n):
+            if A[i] ^ A[-1]:
+        n = len(A)
+        left, right = 0, n - 1
+class Solution:
+    def maxDistance(self, A: List[int]) -> int:
