f22482ddb6be51f408e69d1bef017e5e30b9be06
Shresth Shakrawar <shresthshakrawar55@gmail.com>
2026-05-13 21:42:13 +0530

    Add solution for 647. Palindromic Substrings

---
+++ medium/647-palindromic-substrings/647-palindromic-substrings.py
@@ -0,0 +1,10 @@
+                l-=1
+                r+=1
+            return res
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                res += 1
+            res = 0
+        def countPali(self,s,l,r):
+        ans = 0
+    def countSubstrings(self, s: str) -> int:
+class Solution:
